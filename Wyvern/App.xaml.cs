ab5cb2343155ea498ab72128eccd50d1a8968c4e
echel0n <kevin.s.anthony@gmail.com>
2017-07-05 16:23:10 -0400

    Made App Singleton.
    This will prevent too much API Spamming if user clicks app multiple times

--- Wyvern/App.xaml.cs
+++ Wyvern/App.xaml.cs
@@ -1,6 +1,12 @@
 ﻿#region Using
 
+using System;
+using System.Reflection;
+using System.Threading;
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Navigation;
+using Noside.Common.Helpers;
 
 #endregion
 
@@ -11,5 +17,61 @@ namespace Noside
     /// </summary>
     public partial class App : Application
     {
+        #region Fields
+
+        private uint _message;
+        private Mutex _mutex;
+        // ReSharper disable once RedundantDefaultMemberInitializer
+        // Explicit is better than implicit
+        private bool _hooked = false;
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            bool mutexCreated;
+            string mutexName = $"Local\\{assembly.GetType().GUID}{assembly.GetName().Name}";
+            this._mutex = new Mutex(true, mutexName, out mutexCreated);
+            this._message = NativeMethods.User32.RegisterWindowMessage(mutexName);
+
+            if (!mutexCreated)
+            {
+                NativeMethods.User32.PostMessage(NativeMethods.HwndBroadcast, this._message, IntPtr.Zero, IntPtr.Zero);
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            if (this._hooked) return;
+            HwndSource hwndSource = HwndSource.FromHwnd(new WindowInteropHelper(this.MainWindow).Handle);
+            hwndSource?.AddHook(this.HandleMessages);
+            this._hooked = true;
+        }
+
+        private IntPtr HandleMessages(IntPtr handle, Int32 message, IntPtr wParameter, IntPtr lParameter, ref Boolean handled)
+        {
+            if (message == this._message)
+            {
+                if (this.MainWindow.WindowState == WindowState.Minimized)
+                    this.MainWindow.WindowState = WindowState.Normal;
+
+                bool topmost = this.MainWindow.Topmost;
+
+                this.MainWindow.Topmost = true;
+                this.MainWindow.Topmost = topmost;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        #endregion
     }
 }
---
+++ Wyvern/Common/Helpers/NativeMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noside.Common.Helpers
+{
+    public static class NativeMethods
+    {
+
+        public static readonly IntPtr HwndBroadcast = new IntPtr(0xffff);
+
+        public static class User32
+        {
+            [DllImport("user32.dll", SetLastError =true, CharSet = CharSet.Auto)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool PostMessage(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            public static extern uint RegisterWindowMessage(string lpstring);
+        }
+    }
+}
