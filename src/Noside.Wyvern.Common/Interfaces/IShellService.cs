09f48af02f5b9542972e5163b7e4ef78e54bc8c6
echel0n <kevin.s.anthony@gmail.com>
2018-03-21 21:40:08 -0400

    Moved from WindowManager to Shell Service.  This now propery opens windows.

---
+++ src/Noside.Wyvern.Common/Interfaces/IShellService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using Prism.Regions;
+
+namespace Noside.Wyvern {
+	internal interface IShellService {
+		#region Public Methods
+		
+		void ShowShell<T>() where T : Window;
+
+		#endregion
+	}
+}
---
+++ src/Noside.Wyvern.Common/ShellService.cs
@@ -0,0 +1,38 @@
+﻿#region Using
+
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Regions;
+
+#endregion
+
+namespace Noside.Wyvern {
+	internal class ShellService : IShellService {
+		#region Fields
+
+		private readonly IRegionManager _regionManager;
+		private readonly IUnityContainer _unityContainer;
+
+		#endregion
+
+		#region Constructors and Destructors
+
+		public ShellService(IUnityContainer unityContainer, IRegionManager regionManager) {
+			this._unityContainer = unityContainer;
+			this._regionManager = regionManager;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		
+		public void ShowShell<T>() where T : Window {
+			T shell = this._unityContainer.Resolve<T>();
+			IRegionManager scopedRegion = this._regionManager.CreateRegionManager();
+			RegionManager.SetRegionManager(shell, scopedRegion);
+			shell.Show();
+		}
+		#endregion
+	}
+}
